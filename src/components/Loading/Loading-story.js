51dc0168646b1c607438511c7295c1e7a6401bb9
loganmccaul <loganmccaul@us.ibm.com>
2018-06-19 17:13:31 -0500

    chore(loading): fix text in storyboard description

--- src/components/Loading/Loading-story.js
+++ src/components/Loading/Loading-story.js
@@ -5,7 +5,7 @@ import Loading from '../Loading';
 storiesOf('Loading', module).addWithInfo(
   'Default Loading',
   `The loading component is meant to replace the teal spinner for static page loads in the IBM Cloud platform.
-  This spinner should be used in context and not tied to an overlay. For dynamic data loading use carbon skeleton components.`,
+  This loading pattern should be used in context and not tied to an overlay. For dynamic data loading use carbon skeleton components.`,
   () => (
     <div>
       <Loading style={{ margin: '200px auto' }} />
