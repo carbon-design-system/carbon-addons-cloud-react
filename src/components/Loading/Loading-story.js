459904ba76949564025a1ec4ae457643a4347489
loganmccaul <loganmccaul@us.ibm.com>
2018-06-19 16:52:15 -0500

    chore(cloudloading): add storyboard details

--- src/components/Loading/Loading-story.js
+++ src/components/Loading/Loading-story.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Loading from '../Loading';
 
-storiesOf('Loading', module).addWithInfo('Default Loading', ``, () => (
-  <div>
-    <Loading style={{ margin: '200px auto' }} />
-  </div>
-));
+storiesOf('Loading', module).addWithInfo(
+  'Default Loading',
+  `The loading component is meant to replace the teal spinner for static page loads in the IBM Cloud platform.
+  This spinner should be used in context and not tied to an overlay. For dynamic data loading use carbon skeleton components.`,
+  () => (
+    <div>
+      <Loading style={{ margin: '200px auto' }} />
+    </div>
+  )
+);
