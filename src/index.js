77a4e35eda9ea6e7a1d7fab76c9f04d185fafda8
Tatiana Moreira <tatianamoreira@MacBook-Pro-de-Tatiana.local>
2018-11-12 08:57:53 -0600

    fix(filters): new nested filterable multiselect added

--- src/index.js
+++ src/index.js
@@ -33,3 +33,4 @@ export { default as ResourceHeader } from './components/ResourceHeader';
 
 export TagList from './components/TagList';
 export Tag from './components/Tag';
+export NestedFilterableMultiSelect from './components/NestedFilterableMultiSelect';
